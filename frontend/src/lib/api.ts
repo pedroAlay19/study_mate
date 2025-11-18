b99baf0efdd25fb99ee2ed905dc5e694dea1d9b2
saulcm555 <saulcastrocm@hotmail.com>
2025-11-18 13:30:01 -0500

    Eliminando el redireccionamiento al localhost

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3007',
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 // Interceptor para agregar token automáticamente a todas las peticiones
