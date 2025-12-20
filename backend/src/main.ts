cd08860e6ac685a7c9054049df75db68a127c2b7
pedroAlay19 <e1312731894@live.uleam.edu.ec>
2025-12-19 19:01:43 -0500

    fix3

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   // Puerto para Cloud Run
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 80;
   await app.listen(port, '0.0.0.0');   
   console.log(`Server running on port ${port}`);
 }
